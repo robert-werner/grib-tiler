f1d6195f0031c2d1390f4dcabeb9231a7f40ac7c
Leonid Kolesnichenko <xperience439@gmail.com>
2023-02-17 15:20:06 +0300

    Add meta.info JSON writing (fix)

--- grib_tiler_cli.py
+++ grib_tiler_cli.py
@@ -1,3 +1,4 @@
+import json
 import os
 import sys
 import tempfile
@@ -24,6 +25,18 @@ EPSG_4326 = CRS.from_epsg(4326)
 EPSG_4326_BOUNDS = list(EPSG_4326.area_of_use.bounds)
 
 temp_dir = tempfile.TemporaryDirectory()
+
+META_INFO = {
+    "meta": {
+        "common": [
+            {"rstep": 0, "rmin": 0},
+            {"gstep": 0, "gmin": 0},
+            {"bstep": 0, "bmin": 0},
+            {"astep": 0, "amin": 0}
+        ]
+    }
+}
+
 
 
 def warp_input(input_subsets, cutline_filename, cutline_layer, output_crs, threads):
@@ -110,6 +123,7 @@ def prepare_for_tiling(warped_subsets, in_ranges, threads):
                                      rasters_for_tiling_tasks,
                                      max_workers=threads,
                                      desc='Рендеринг преобразованных каналов для тайлирования')
+
     return rasters_for_tiling
 
 
@@ -128,12 +142,25 @@ def prepare_in_ranges(rasters_for_tiling, threads):
 def prepare_tiling_tasks(rasters_for_tiling,
                          tiles, output,
                          tms, tilesize,
-                         image_format, generate_nodata_mask=True):
+                         image_format, in_ranges, generate_nodata_mask=True):
     tiling_tasks = []
     nodata_mask = None
     if generate_nodata_mask:
         nodata_mask = np.zeros((tilesize, tilesize), dtype='uint8')
-    for raster_for_tiling in rasters_for_tiling:
+    for raster_for_tiling, in_ranges in zip(rasters_for_tiling, in_ranges):
+        subdirectory_name = os.path.splitext(os.path.basename(raster_for_tiling))[0]
+        os.makedirs(os.path.join(output, subdirectory_name), exist_ok=True)
+        meta_json_filename = os.path.join(output, subdirectory_name, 'meta.json')
+        meta_info = META_INFO
+        _in_ranges = in_ranges
+        if isinstance(_in_ranges[0], float):
+            _in_ranges = [_in_ranges]
+        for idx, band, in_range in zip(range(0, len(_in_ranges) + 1),
+                                       ['r', 'g', 'b', 'a'][0:len(_in_ranges)], _in_ranges):
+            meta_info['meta']['common'][idx][f'{band}step'] = (in_range[1] - in_range[0]) / 255
+            meta_info['meta']['common'][idx][f'{band}min'] = in_range[0]
+        with open(meta_json_filename, 'w') as meta_json:
+            json.dump(meta_info, meta_json, indent=4)
         for tile in tiles:
             tiling_task = RenderTileTask(input_filename=raster_for_tiling,
                                          output_directory=output,
@@ -214,7 +241,7 @@ def grib_tiler(input_filename,
         else:
             in_ranges = prepare_in_ranges(uv_subsets, threads)
             rasters_for_tiling = prepare_for_tiling(uv_subsets, in_ranges, threads)
-        tiling_tasks = prepare_tiling_tasks(rasters_for_tiling, tiles, output, tms, tilesize, image_format)
+        tiling_tasks = prepare_tiling_tasks(rasters_for_tiling, tiles, output, tms, tilesize, image_format,  in_ranges)
     else:
         band_numbers = bands_handler(band_numbers)
         if not band_numbers:
@@ -249,7 +276,7 @@ def grib_tiler(input_filename,
         else:
             in_ranges = prepare_in_ranges(band_subsets, threads)
             rasters_for_tiling = prepare_for_tiling(band_subsets, in_ranges, threads)
-        tiling_tasks = prepare_tiling_tasks(rasters_for_tiling, tiles, output, tms, tilesize, image_format,
+        tiling_tasks = prepare_tiling_tasks(rasters_for_tiling, tiles, output, tms, tilesize, image_format, in_ranges,
                                             generate_nodata_mask=False)
     process_map(render_tile, tiling_tasks,
                 max_workers=threads,
